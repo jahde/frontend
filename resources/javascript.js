dbd3cd0125560f8321b428df350634b0c5dbcbef
Jahde <jahde.hiywan@gmail.com>
2014-11-15 13:34:17 -0500

    javascript and jquery files

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,14 @@
 // Your Javascript will go here!
+$(document).ready(function() {
+// jQuery use
+//  $("#copyright").click(function(){
+//    $(this).css('color','purple');
+//  })
+// javascript use
+  document.getElementById('copyright').onclick = function () {
+    this.style.color = 'purple';
+  }
+});
+
+
+
